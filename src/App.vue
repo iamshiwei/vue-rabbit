7e111f2a8313e37711d7f5ed7288667d820e2391
iamshiwei <2587159769@qq.com>
2023-08-05 16:49:46 +0800

    axios 封装

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,7 @@ import {RouterView} from 'vue-router'
 
 <template>
   <div>
-    <el-button type="primary">Primary</el-button>
-    <el-button type="success">Success</el-button>
-    <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button>
+    <el-button type="primary">Primary</el-button>a
     <el-button type="danger">Danger</el-button>
   </div>
 
--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 
 
 import App from './App.vue'
 import router from './router'
 
+import {getCategory} from '@/apis/testapi.js'
+
+getCategory().then(res => {
+  console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())

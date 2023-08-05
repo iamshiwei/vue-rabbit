0ea32ce6986311487114112be77380c091ecd587
iamshiwei <2587159769@qq.com>
2023-08-05 16:35:19 +0800

    修改elementplus主题色

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <script setup>
-import { RouterView } from 'vue-router'
-</script> 
-   
+import {RouterView} from 'vue-router'
+</script>
+
 <template>
-  <div> 
-
-
-
-
-    
+  <div>
     <el-button type="primary">Primary</el-button>
     <el-button type="success">Success</el-button>
     <el-button type="info">Info</el-button>
-    <el-button type="warning">Warning</el-button> 
+    <el-button type="warning">Warning</el-button>
     <el-button type="danger">Danger</el-button>
   </div>
 
-  <RouterView />
+  <RouterView/>
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -13,9 +13,20 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      // 配置elementplus采用scss的样式配置系统
+      resolvers: [ElementPlusResolver({importStyle:'sass'})]
     })
   ],
+  css: {
+    // 2. 自动导入定制化样式文件进行样式覆盖
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          `
+      }
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
